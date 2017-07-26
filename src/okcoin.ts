826f67c8ad80f9fc9aa49a1b372ad4d53ad0a118
destec <catalyst.zhang@gmail.com>
2017-07-26 22:08:10 +0800

    add request function as class function

--- src/okcoin.ts
+++ src/okcoin.ts
@@ -1,10 +1,16 @@
+import * as path from 'path';
+import * as url from 'url';
+import { assign } from 'lodash';
 import { Logger, LoggerInstance, transports } from 'winston';
+import { request } from 'urllib';
 
 interface IOkcoin {
   apiKey: string;
   secretKey: string;
   logger: LoggerInstance;
 }
+
+const Request = Symbol('NODE-OKCOIN#REQUEST');
 
 export default class Okcoin implements IOkcoin {
   public apiKey;
@@ -20,5 +26,31 @@ export default class Okcoin implements IOkcoin {
         })
       ]
     });
+  }
+
+  [Request](uri, query, customOption) {
+    const urlObj: url.URL = {
+      protocol: 'https',
+      host: 'www.okcoin.com',
+      pathname: path.join('/api/v1', uri),
+      query: query
+    };
+    const endpoint: string = url.format(urlObj);
+    const defaultOption = {
+      contentType: 'json',
+      dataType: 'json',
+      timeout: 5000,
+      timing: true
+    };
+    let option = defaultOption;
+    if (customOption) option = assign(defaultOption, customOption);
+    return request(endpoint, option)
+      .catch((err: Error) => {
+        this.logger.warn(err);
+      })
+      .then(res => {
+        this.logger.info(uri, res.res.timing);
+        return res.data;
+      });
   }
 }
