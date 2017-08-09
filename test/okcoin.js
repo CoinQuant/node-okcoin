e1fa764fcca41f195e80759ef4d087f390476d6f
destec <catalyst.zhang@gmail.com>
2017-08-09 08:51:37 +0800

    add normal unit tests

--- test/okcoin.js
+++ test/okcoin.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const assert = require('chai').assert;
+const moment = require('moment');
 
 describe('node-okcoin', () => {
   describe('#getTicker', () => {
@@ -9,6 +10,20 @@ describe('node-okcoin', () => {
       const okcoin = new Okcoin();
       okcoin.getTicker().catch(err => {
         assert.equal(err, 'market symbol is required');
+        done();
+      });
+    });
+    it('should return correct data', done => {
+      const Okcoin = require('../index').default;
+      const okcoin = new Okcoin();
+      okcoin.getTicker('anscny').then(res => {
+        assert.isTrue(res.date - moment().unix() < 100);
+        assert.isTrue(+res.ticker.buy > 0);
+        assert.isTrue(+res.ticker.high > 0);
+        assert.isTrue(+res.ticker.last > 0);
+        assert.isTrue(+res.ticker.low > 0);
+        assert.isTrue(+res.ticker.sell > 0);
+        assert.isTrue(+res.ticker.vol >= 0);
         done();
       });
     });
